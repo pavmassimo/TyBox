58edf32a55ddc5dc6acaec1687832e785b81b96b
Marco Lacava <61839160+LacavaMarco@users.noreply.github.com>
2023-10-17 18:33:23 +0200

    Delete old TyBox directory

--- TyBox/Activations/__init__.py
+++
@@ -1,5 +0,0 @@
-from .Activation import *
-from .Linear import *
-from .Relu import *
-from .Softmax import *
-from .Sigmoid import *
--- TyBox/Layers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .Add import *
-from .Conv2d import *
-from .AvgPool2d import *
-from .Dense import *
-from .MaxPool2d import *
-from .Relu import *
-from .Layer import *
-
